8d2b190d29ce0f8e49c6911ead666ce6bdff9e5a
Matej Poklemba <matejpoklemba@gmail.com>
2017-11-04 00:34:24 +0100

    Ukol 3

---
+++ app/src/main/java/cz/muni/fi/pv256/movio2/uco_422476/MainFragment.java
@@ -0,0 +1,97 @@
+package cz.muni.fi.pv256.movio2.uco_422476;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Matej on 3.11.2017.
+ */
+
+public class MainFragment extends Fragment {
+
+    private static final String TAG = MainFragment.class.getSimpleName();
+    private static final String SELECTED_KEY = "selected_position";
+
+    private int mPosition = ListView.INVALID_POSITION;
+    private OnFilmSelectListener mListener;
+    private Context mContext;
+    private RecyclerView mListView;
+
+    @Override
+    public void onAttach(Context activity) {
+        super.onAttach(activity);
+
+        try {
+            mListener = (OnFilmSelectListener) activity;
+        } catch (ClassCastException e) {
+            Log.e(TAG, "Activity must implement OnFilmSelectListener", e);
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+
+        mListener = null;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mContext = getActivity().getApplicationContext();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        final ArrayList<Film> filmList = ((MainActivity)getActivity()).getFilmList();
+        SetListener((Button) view.findViewById(R.id.button1), 0, filmList);
+        SetListener((Button) view.findViewById(R.id.button2), 1, filmList);
+        SetListener((Button) view.findViewById(R.id.button3), 2, filmList);
+
+        if (savedInstanceState != null && savedInstanceState.containsKey(SELECTED_KEY)) {
+            mPosition = savedInstanceState.getInt(SELECTED_KEY);
+
+            if (mPosition != ListView.INVALID_POSITION) {
+                //mListView.smoothScrollToPosition(mPosition);
+            }
+        }
+
+        return view;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        if (mPosition != ListView.INVALID_POSITION) {
+            outState.putInt(SELECTED_KEY, mPosition);
+        }
+        super.onSaveInstanceState(outState);
+    }
+
+    private void SetListener(Button button, final int position, final ArrayList<Film> filmList) {
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mPosition = position;
+                mListener.onFilmSelect(filmList.get(position));
+            }
+        });
+    }
+
+    public interface OnFilmSelectListener {
+        void onFilmSelect(Film film);
+    }
+}
